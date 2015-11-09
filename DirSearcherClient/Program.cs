da0592f1515e478a2959470d6e6631a56c26cf40
Vittorio Bertocci <vibro@libero.it>
2015-11-09 14:12:33 -0800

    Renaming and updates to Beta8

--- DirSearcherClient/Program.cs
+++ DirSearcherClient/Program.cs
@@ -9,28 +9,30 @@ using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace active_directory_dotnet_deviceprofile
+namespace DirSearcherClient
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        //public const string clientId = "04b07795-8ddb-461a-bbee-02f9e1bf7b46";
+        public const string resource = "00000002-0000-0000-c000-000000000000";
+        public const string clientId = "b78054de-7478-45a6-be1c-09f696a91d64";
+        public void Main(string[] args)
         {
-
             string commandString = string.Empty;
             Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("**************************************************");
-            Console.WriteLine("*        Directory Searcher Text Client          *");
-            Console.WriteLine("*                                                *");
-            Console.WriteLine("*        Type commands to search users           *");
-            Console.WriteLine("*                                                *");
-            Console.WriteLine("**************************************************");
+            Console.WriteLine("***********************************************************");
+            Console.WriteLine("*             Directory Searcher Text Client              *");
+            Console.WriteLine("*                                                         *");
+            Console.WriteLine("*             Type commands to search users               *");
+            Console.WriteLine("*                                                         *");
+            Console.WriteLine("***********************************************************");
             Console.WriteLine("");
 
             // main command cycle
             while (!commandString.Equals("Exit"))
             {
                 Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine("Enter command (search | clear | exit | help) >");
+                Console.WriteLine("Enter command (search | clear | printcache | exit | help) >");
                 commandString = Console.ReadLine();
 
                 switch (commandString.ToUpper())
@@ -41,13 +43,17 @@ namespace active_directory_dotnet_deviceprofile
                     case "HELP":
                         Help();
                         break;
+                    case "PRINTCACHE":
+                        PrintCache();
+                        break;
                     case "EXIT":
-                        Console.WriteLine("Bye!"); ;
+                        Console.WriteLine("Bye!");
+                        return;
                         break;
                     default:
                         if (commandString.ToUpper().StartsWith("SEARCH") && commandString.Split(' ').Count() > 1)
                         {
-                            string[] localArgs = commandString.Split(' ');
+                            string[] localArgs = commandString.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
                             if (localArgs.Count() > 2)
                                 Search(localArgs[1], localArgs[2]);
                             else
@@ -120,6 +126,23 @@ namespace active_directory_dotnet_deviceprofile
                 Console.WriteLine("Failed to obtain a token.");
             }
         }
+        static void PrintCache()
+        {
+            AuthenticationContext ctx = new AuthenticationContext("https://login.microsoftonline.com/common");
+            var cacheContent = ctx.TokenCache.ReadItems();
+            Console.ForegroundColor = ConsoleColor.Green;
+            if (cacheContent.Count() > 0)
+            {
+                Console.WriteLine("{0,-30} | {1,-15}", "UPN", "TenantId");
+                Console.WriteLine("-----------------------------------------------------------------");
+                foreach (TokenCacheItem tci in cacheContent)
+                {
+                    Console.WriteLine("{0,-30} | {1,-15}  ", tci.DisplayableId, tci.TenantId);
+                }
+                Console.WriteLine("-----------------------------------------------------------------");
+            }
+            else { Console.WriteLine("The cache is empty."); }
+        }
         static void ClearCache()
         {
             AuthenticationContext ctx = new AuthenticationContext("https://login.microsoftonline.com/common");
@@ -132,6 +155,7 @@ namespace active_directory_dotnet_deviceprofile
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("SEARCH  - searches for a user by alias. Requires sign in. \nUsage: \nsearch <useralias> [domain to search]");
             Console.WriteLine("CLEAR - empties the token cache, allowing you to sign in as a different user");
+            Console.WriteLine("PRINTCACHE - displays the current cache content (basic fields only)");
             Console.WriteLine("HELP  - displays this page");
             Console.WriteLine("EXIT  - closes this program");
             Console.WriteLine("");
@@ -139,8 +163,6 @@ namespace active_directory_dotnet_deviceprofile
 
         static AuthenticationResult GetToken(string tenant)
         {
-            string resource = "00000002-0000-0000-c000-000000000000";
-            string clientId = "04b07795-8ddb-461a-bbee-02f9e1bf7b46";
             AuthenticationContext ctx = null;
             if (tenant != null)
                 ctx = new AuthenticationContext("https://login.microsoftonline.com/" + tenant);
@@ -160,14 +182,39 @@ namespace active_directory_dotnet_deviceprofile
             }
             catch (Exception exc)
             {
+                var adalEx = exc.InnerException as AdalException;
+                if ((adalEx != null) && (adalEx.ErrorCode == "failed_to_acquire_token_silently"))
+                {
+                    result = GetTokenViaCode(ctx);
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Something went wrong.");
+                    Console.WriteLine("Message: " + exc.InnerException.Message + "\n");
+                }
+            }
+            return result;
+
+        }
+        static AuthenticationResult GetTokenViaCode(AuthenticationContext ctx)
+        {
+            AuthenticationResult result = null;
+            try
+            {
                 DeviceCodeResult codeResult = ctx.AcquireDeviceCodeAsync(resource, clientId).Result;
                 Console.ForegroundColor = ConsoleColor.White;
                 Console.WriteLine("You need to sign in.");
                 Console.WriteLine("Message: " + codeResult.Message + "\n");
                 result = ctx.AcquireTokenByDeviceCodeAsync(codeResult).Result;
             }
+            catch (Exception exc)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Something went wrong.");
+                Console.WriteLine("Message: " + exc.Message + "\n");
+            }
             return result;
         }
     }
 }
-
